934832c066723dba2da1f0dd46104fd2ffe75b21
khushi-solanki09 <khushisolanki2920@gmail.com>
2026-08-12 00:55:22 +0530

    connecting database

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import mongoose  from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express"
+
+const app = express()
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error", error)
+            throw err
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`app is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("Error : ", error)
+        throw err
+    }
+})()
